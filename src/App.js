b72e0a1169debaffb30abd066f69e118a0fd5ec8
Christopher <leechrs1@gmail.com>
2024-01-27 21:25:48 -0600

    added useState to the Previous and Next buttons

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
+import { useState } from "react";
+
 const messages = [
   "Learn React 🌟",
   "Apply for jobs 💼",
   "Invest your new income 🤑"
 ];
-
+// useState(1) = 1 is the default value
 export default function App() {
-  const step = 1;
-
+  const [step, setStep] = useState(1)
+  //useState is a React hook due to the keyword use
   function handlePrevious() {
-    
+    if (step > 1) setStep(step - 1);
   }
 
   function handleNext() {
-    
+    if (step < 3) setStep(step + 1);
   }
 
   return (
   <div className="steps">
     <div className="numbers">
-      <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
-      <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
-      <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
+      <div className={step >= 1 ? 'active' : ''}>1</div>
+      <div className={step >= 2 ? 'active' : ''}>2</div>
+      <div className={step >= 3 ? 'active' : ''}>3</div>
     </div>
 
     <p className="message">Step {step}: {messages[step - 1]}</p>
